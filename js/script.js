579114bd6bf8974069446e9447e29c4f4987f640
Herman461 <zhukovskiyherman@gmail.com>
2022-08-19 12:12:00 +0300

    build: update calculator block

--- js/script.js
+++ js/script.js
@@ -397,3 +397,10 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll');
   }
 });
+var picker = datepicker('#calendar', {
+  formatter: function formatter(input, date, instance) {
+    input.value = date.toLocaleDateString().replace(/\./g, '/');
+  },
+  customMonths: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+  customDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+});
